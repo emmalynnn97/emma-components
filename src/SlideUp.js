807ff8b431be30fea72831697ad601f9d7eb8898
emmalynnn97 <emmalynnn97@gmail.com>
2020-06-13 12:46:26 -0700

    i have half a working slider, still needs fixing

--- src/SlideUp.js
+++ src/SlideUp.js
@@ -3,65 +3,142 @@ import React, { Component } from 'react'
 export class SlideUp extends Component {
     constructor(props) {
         super(props)
-    
+
         this.state = {
-             slideDist:30,
-             opacity:0,
-             slides:this.props.slides
+            slideDist: 30,
+            opacity: 0,
+            slides: this.props.slides,
+            xOffset: 0
         }
         this.isElementXPercentInViewport = this.isElementXPercentInViewport.bind(this);
     }
-    componentDidMount(){
-        window.addEventListener('scroll',()=>{
+    componentDidMount() {
+        var slideUps = document.querySelectorAll('.slide-up');
+        var numNextHidden = 0;
+        for (var i = 0; i < slideUps.length; i++) {
+            if (i > 2) {
+                slideUps[i].style.width = '0'
+                slideUps[i].style.padding = '0'
+                slideUps[i].style.opacity='0'
+                slideUps[i].style.visibility='hidden'
+            }
+        }
+        document.querySelector('.next-button').addEventListener('click', () => {
+            numNextHidden++;
+            for (var i = 0; i < slideUps.length; i++) {
+                if (i < numNextHidden) {
+                    slideUps[i].style.width = '0'
+                    slideUps[i].style.padding = '0'
+                    slideUps[i].style.opacity='0'
+                    slideUps[i].style.visibility='hidden'
+                }
+                if (i === slideUps.length - numNextHidden) {
+                    slideUps[i].style.width = '33%'
+                    slideUps[i].style.padding = '5px'
+                    slideUps[i].style.visibility='visible'
+                }
+            }
+        })
+        document.querySelector('.prev-button').addEventListener('click', () => {
+            for (var i = 0; i < slideUps.length; i++) {
+                if (i === 2) {
+                    slideUps[i].style.width = '0'
+                    slideUps[i].style.padding = '0'
+                    slideUps[i].style.opacity='0'
+                    slideUps[i].style.visibility='hidden'
+                }
+                if (i === slideUps.length - 2 || i ===0) {
+                    slideUps[i].style.width = '33%'
+                    slideUps[i].style.padding = '5px'
+                    slideUps[i].style.visibility='visible'
+                    slideUps[i].style.opacity='1'
+                }
+            }
+        })
+        window.addEventListener('scroll', () => {
             let slideUpContainers = document.querySelectorAll('.slide-up-container');
-            slideUpContainers.forEach((slideUpContainer)=>{
-                if(this.isElementXPercentInViewport(slideUpContainer,70)){
-                    this.setState({slideDist:0,opacity:1})
+            slideUpContainers.forEach((slideUpContainer) => {
+                if (this.isElementXPercentInViewport(slideUpContainer, 70)) {
+                    this.setState({ slideDist: 0, opacity: 1 })
                 }
             })
         })
     }
-    componentWillUnmount(){
+    componentWillUnmount() {
         window.removeEventListener('scroll');
     }
     isElementXPercentInViewport(el, percentVisible) {
         let
-          rect = el.getBoundingClientRect(),
-          windowHeight = (window.innerHeight || document.documentElement.clientHeight);
-            
+            rect = el.getBoundingClientRect(),
+            windowHeight = (window.innerHeight || document.documentElement.clientHeight);
         return !(
-          Math.floor(100 - (((rect.top >= 0 ? 0 : rect.top) / +-(rect.height / 1)) * 100)) < percentVisible ||
-          Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) < percentVisible
+            Math.floor(100 - (((rect.top >= 0 ? 0 : rect.top) / +-(rect.height / 1)) * 100)) < percentVisible ||
+            Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) < percentVisible
         )
-      };
+    };
     render() {
-        const containerStyle={
-            width:'100%',
-            backgroundColor:'rgba(0,0,70,.2)',
-            display:'flex',
-            overflowY:'hidden',
-            justifyContent:'center',
-            alignItems:'center',
-            flexDirection:'column',
-            
+        const slideUpStyle = {
+            height: '25vh',
+            width: '33%',
+            transition: '.45s ease',
+            padding: '5px',
+            transform: `translateX(${this.state.xOffset}%) translateY(${this.state.slideDist}vh)`,
+            opacity: this.state.opacity
         }
-        const slideUpStyle={
-            height:'25vh',
-            width:'37.5vh',
-            margin:'5vh 1vw',
-            transition:'.45s ease',
-            transform:`translateY(${this.state.slideDist}vh)`,
-            opacity:this.state.opacity
+        const slideContainerStyle = {
+            display: 'flex',
+            width: '100%',
+            padding:'0px 100px',
+            overflowX: 'hidden',
+            justifyContent: 'center'
         }
-        const slideContainerStyle={
-            display:'flex',
+        const nextStyle = {
+            width: '50px',
+            backgroundColor: 'rgb(0,0,20)',
+            color: 'white',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: '34px',
+            lineHeight: '1.5',
+            cursor: 'pointer',
+            transition: '.35s ease',
+            zIndex: '2'
+        }
+        const prevStyle = {
+            width: '50px',
+            backgroundColor: 'rgb(0,0,20)',
+            color: 'white',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: '34px',
+            lineHeight: '1.5',
+            cursor: 'pointer',
+            transition: '.35s ease',
+            zIndex: '2'
+        }
+        const innerContentStyle = {
+            display: 'flex',
+            overflowY: 'hidden',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            width: '100%',
+            backgroundColor: 'rgba(0,0,70,.2)',
         }
         return (
-            <div className='slide-up-container' style={containerStyle}>
-                <h2>Recent Projects</h2>
-                <div className='slide-container' style={slideContainerStyle}>{
-                    this.state.slides.map(slide=><div className='slide-up' style={slideUpStyle}>{slide}</div>)
-                }</div>
+            <div className='slide-up-container'>
+                <div style={innerContentStyle}>
+                    <div style={{ width: '95%', display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start', position: 'relative', top: '30vh' }}>
+                        <div style={nextStyle} class='next-button'>&#171;</div>
+                        <div style={prevStyle} class='prev-button'>&#187;</div>
+                    </div>
+                    <h2>Recent Projects</h2>
+                    <div className='slide-container' style={slideContainerStyle}>
+                        {this.state.slides.map(slide => <div className='slide-up' style={slideUpStyle}>{slide}</div>)}
+                    </div>
+                </div>
             </div>
         )
     }
--- src/Template.js
+++ src/Template.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import Hero from './Hero'
 import SlideUp from './SlideUp'
-import SlideIn from './SlideIn'
 import Footer from './Footer'
+
 export class Template extends Component {
     constructor(props) {
         super(props)
@@ -42,13 +42,28 @@ export class Template extends Component {
             backgroundImage:'url("https://imgix.cosmicjs.com/6561a430-ad9d-11ea-8ffe-4bdcca0409c6-Screen-Shot-2020-06-13-at-10.41.49-AM.png")',
             backgroundSize:'cover',
             backgroundPosition:'center'
+        }}></div>,
+        <div style={{
+            height:'100%',
+            width:'100%',
+            backgroundImage:'url("https://imgix.cosmicjs.com/5553d8b0-8f52-11ea-847f-0d558a1b2ff3-Screen-Shot-2020-04-05-at-7.07.46-PM.png")',
+            backgroundSize:'cover',
+            backgroundPosition:'center'
+        }}></div>,
+        <div style={{
+            height:'100%',
+            width:'100%',
+            backgroundImage:'url("https://imgix.cosmicjs.com/ce21a580-8e79-11ea-bd26-81830172bbcb-Screen-Shot-2020-04-05-at-7.26.33-PM.png")',
+            backgroundSize:'cover',
+            backgroundPosition:'center'
         }}></div>
+        
     ]
         return (
             <div>
                 <Hero content={heroContent} overlayColor='rgba(0,0,70,.2)' height='87.5vh' parallax={true} image='https://imgix.cosmicjs.com/ebe9b000-ad49-11ea-8ffe-4bdcca0409c6-background-bloom-blooming-blossom-583850.jpg'/>
                 <SlideUp slides={slides} />
-                <SlideIn/>
+                {/*<SlideIn/>*/}
                 <Footer content={<><h2 style={{margin:'0 2.5vw',color:'white'}}>Emma Lynn Web Development</h2><a rel="noopener noreferrer" target="_blank" href="https://github.com/emmalynnn97"><img src="https://cosmic-s3.imgix.net/4ae87a70-a3b1-11ea-8370-b9ea0c50cbb0-GitHub-Mark-Light-32px.png" alt="github logo" /></a></>} bgColor='linear-gradient(180deg,rgba(15,0,80,1),rgba(15,0,80) )'/>
             </div>
         )
